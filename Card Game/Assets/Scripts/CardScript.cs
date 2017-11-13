bc3ac94656bc19f55670c8dbf7a095b8834490d5
Alexis Claude André Dewaele <alexis.dewaele@me.com>
2017-11-13 19:43:32 +0100

    Adds CardScript and DropZoneScript as well as a prefab for Card

---
+++ Card Game/Assets/Scripts/CardScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Card : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
+
+	public Transform parentToReturnTo = null;
+
+	private string cardName;
+	private string descriptionText;
+
+	public Card(string name, string description) {
+		cardName = name;
+		descriptionText = description;
+	}
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		Debug.Log ("Begin Drag");
+
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent(parentToReturnTo.parent);
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		Debug.Log ("Dragging");
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		Debug.Log ("End Drag");
+		this.transform.SetParent (parentToReturnTo);
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+	}
+}
---
+++ Card Game/Assets/Scripts/DropZoneScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drop : MonoBehaviour, IDropHandler {
+
+	public void OnDrop(PointerEventData eventData) {
+		Debug.Log (eventData.pointerDrag.name + "was dropped on " + gameObject.name);
+
+		Card c = eventData.pointerDrag.GetComponent<Card> ();
+		if (c != null) {
+			c.parentToReturnTo = this.transform;
+		}
+	}
+}
