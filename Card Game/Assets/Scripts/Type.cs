879d8ba058db65cfb84034b8476fe66824662881
Andrea Veneziano <andrea.veneziano@epfl.ch>
2017-11-13 19:33:57 +0100

    Added new basic scripts
    Scripts Building, Card, Faction, Special, Type, Unit created

---
+++ Card Game/Assets/Scripts/Type.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Type : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+			
+	}
+}
